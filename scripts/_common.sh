26ff86c0557b14830a696227c7aed3534c9636a1
axolotle <nchesnais@mailoo.org>
2021-10-07 16:53:44 +0200

    update install with repo args

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,11 +6,16 @@
 
 # dependencies used by the app
 pkg_dependencies="python3-venv python3-pip"
+postgresql_dependencies="postgresql postgresql-server-dev-all"
+sqlite_dependencies="sqlite3"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
+ynh_get_arg () {
+  arg=$(ynh_read_manifest "$final_path/ynh.json" $1)
+  [[ "$arg" == "null" ]] && echo $2 || echo $arg
+}
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
